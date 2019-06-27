4d86553b8d0353cc4e80d73ca1e6d605c1cf6b88
Mohamed Gamal Masood Abou-alela <Mohamed.G70500@Egypt.TE-Data.core>
2019-06-27 13:54:17 +0200

    second commit

---
+++ assets/js/APIS/Pro_signup_api_script.js
@@ -0,0 +1,51 @@
+
+ 
+
+$(function(){
+     //get main category
+ let categoryArr, wrapper = "";        
+ $.get("http://88.80.184.99/tasker/web/api/sub/category", function(data, status){
+     let d = data;
+     categoryArr = d.data;
+     for( let item of categoryArr ){
+     wrapper += `
+         <div class="custom-control custom-checkbox">
+             <input type="checkbox" class="custom-control-input" value="${item.name_en}"  id="${item.name_en}">
+             <label class="custom-control-label" for="${item.name_en}">${item.name_en} </label>
+         </div>
+     `
+     }
+ 
+     
+ });
+    // provider can do popup
+    $("#providersServices").on("click", function(){
+        $("#providersCategories").modal("show").find(".modal-body").html(wrapper)
+    })
+    
+    // provider can do selection
+    $("#providersCategories #Select").on("click", function(e){
+        e.preventDefault();
+        let divContent = "";
+
+        let checkbox = document.querySelectorAll("#providersCategories input[type='checkbox']");
+        for(let checkitem of checkbox){
+            if(checkitem.checked == true ){
+                
+                divContent += `
+                    <div class="item"> ${checkitem.id} </div>
+                `
+            }
+        }
+        $("#providersServices").html(divContent);
+        $("#providersCategories").modal("hide")
+
+
+
+    })
+
+
+   
+ });
+ 
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,7 @@
 $(function(){
    
   
-    // provider can do
-    $("#providersServices").on("click", function(){
-        console.log(1111111111)
-        $("#providersCategories").modal("show")
-    })
-   
+    
 
 
 
