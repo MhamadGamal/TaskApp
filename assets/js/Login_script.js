3277ec32dfb002758ea5f5adc31d79e53adc5934
Mohamed Gamal Masood Abou-alela <Mohamed.G70500@Egypt.TE-Data.core>
2019-07-01 15:31:12 +0200

    fb login

--- assets/js/Login_script.js
+++ assets/js/Login_script.js
@@ -61,104 +61,128 @@ $("#loginbtn").on("click",function (e) {
 });
 $("#fbloginbtn").on("click", function (e) {
     debugger;
-    // initialize and setup facebook js sdk
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: '2750690094972349',
-            xfbml: true,
-            version: 'v2.5'
-        });
-        FB.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-                document.getElementById('status').innerHTML = 'We are connected.';
-                document.getElementById('login').style.visibility = 'hidden';
-            } else if (response.status === 'not_authorized') {
-                document.getElementById('status').innerHTML = 'We are not logged in.'
+         
+    
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: '2750690094972349',
+                autoLogAppEvents : true,
+                xfbml: true,
+                version: 'v2.5'
+            });
+        }
+        FB.login(function(response) {
+            if (response.authResponse) {
+            var access_token =   FB.getAuthResponse()['accessToken'];
+            console.log('Access Token = '+ access_token);
+            FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+            });
             } else {
-                document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+            console.log('User cancelled login or did not fully authorize.');
             }
-        });
-    };
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    // login with facebook with extra permissions
-    function login() {
-        FB.login(function (response) {
-            if (response.status === 'connected') {
-                document.getElementById('status').innerHTML = 'We are connected.';
-                document.getElementById('login').style.visibility = 'hidden';
-            } else if (response.status === 'not_authorized') {
-                document.getElementById('status').innerHTML = 'We are not logged in.'
-            } else {
-                document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
-            }
-        }, { scope: 'email' });
-    }
-
-    // getting basic user info
-    function getInfo() {
-        FB.api('/me', 'GET', { fields: 'first_name,last_name,name,id,picture.width(150).height(150)' }, function (response) {
-            document.getElementById('status').innerHTML = "<img src='" + response.picture.data.url + "'>";
-        });
-    }
+        }, {scope: ''});
 
 
-    var access_token;
-    //FB.login(function (response) {
-    //    debugger;
-    //    if (response.authResponse) {
-    //        access_token = FB.getAuthResponse()['accessToken'];
-    //        console.log('Access Token = ' + access_token);
-    //        FB.api('/me', function (response) {
-    //            console.log('Good to see you, ' + response.name + '.');
-    //        });
-    //    }
-    //    else {
-    //        alert('User cancelled login or did not fully authorize.');
-    //    }
-    //}, { scope: '' });
+    //}
+    // // initialize and setup facebook js sdk
+    // window.fbAsyncInit = function () {
+    //     FB.init({
+    //         appId: '2750690094972349',
+    //         xfbml: true,
+    //         version: 'v2.5'
+    //     });
+    //     FB.getLoginStatus(function (response) {
+    //         if (response.status === 'connected') {
+    //             document.getElementById('status').innerHTML = 'We are connected.';
+    //             document.getElementById('login').style.visibility = 'hidden';
+    //         } else if (response.status === 'not_authorized') {
+    //             document.getElementById('status').innerHTML = 'We are not logged in.'
+    //         } else {
+    //             document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    //         }
+    //     });
+    // };
+    // (function (d, s, id) {
+    //     var js, fjs = d.getElementsByTagName(s)[0];
+    //     if (d.getElementById(id)) { return; }
+    //     js = d.createElement(s); js.id = id;
+    //     js.src = "//connect.facebook.net/en_US/sdk.js";
+    //     fjs.parentNode.insertBefore(js, fjs);
+    // }(document, 'script', 'facebook-jssdk'));
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    // // login with facebook with extra permissions
+    // function login() {
+    //     FB.login(function (response) {
+    //         if (response.status === 'connected') {
+    //             document.getElementById('status').innerHTML = 'We are connected.';
+    //             document.getElementById('login').style.visibility = 'hidden';
+    //         } else if (response.status === 'not_authorized') {
+    //             document.getElementById('status').innerHTML = 'We are not logged in.'
+    //         } else {
+    //             document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    //         }
+    //     }, { scope: 'email' });
+    // }
 
-    if (access_token) {
-        $.ajax({
-            url: "http://88.80.184.99/tasker/web/api/logins",
-            method: 'POST',
-            data: { 'fbtoken': access_token },
-            success: function (result) {
-                debugger;
-                let usertoken = result.data.token;
-                let usertype = result.data.type;
-                if (usertoken && usertype) {
-                    localStorage.setItem('CurrentToken', usertoken);
-                    localStorage.setItem('CurrentUserType', usertype);
-                    email = "";
-                    password = "";
-                    if (localStorage.getItem("CurrentUserType") == "user") {
-                        location.href = '../../index.html';
-                    }
-                }
+    // // getting basic user info
+    // function getInfo() {
+    //     FB.api('/me', 'GET', { fields: 'first_name,last_name,name,id,picture.width(150).height(150)' }, function (response) {
+    //         document.getElementById('status').innerHTML = "<img src='" + response.picture.data.url + "'>";
+    //     });
+    // }
 
-                else {
-                    alert('error');
-                }
 
-            },
-            error: function (result) {
-                alert('error');
-            }
-        });
-    }
+    // var access_token;
+    // //FB.login(function (response) {
+    // //    debugger;
+    // //    if (response.authResponse) {
+    // //        access_token = FB.getAuthResponse()['accessToken'];
+    // //        console.log('Access Token = ' + access_token);
+    // //        FB.api('/me', function (response) {
+    // //            console.log('Good to see you, ' + response.name + '.');
+    // //        });
+    // //    }
+    // //    else {
+    // //        alert('User cancelled login or did not fully authorize.');
+    // //    }
+    // //}, { scope: '' });
+
+    // (function (d, s, id) {
+    //     var js, fjs = d.getElementsByTagName(s)[0];
+    //     if (d.getElementById(id)) { return; }
+    //     js = d.createElement(s); js.id = id;
+    //     js.src = "//connect.facebook.net/en_US/sdk.js";
+    //     fjs.parentNode.insertBefore(js, fjs);
+    // }(document, 'script', 'facebook-jssdk'));
+
+    // if (access_token) {
+    //     $.ajax({
+    //         url: "http://88.80.184.99/tasker/web/api/logins",
+    //         method: 'POST',
+    //         data: { 'fbtoken': access_token },
+    //         success: function (result) {
+    //             debugger;
+    //             let usertoken = result.data.token;
+    //             let usertype = result.data.type;
+    //             if (usertoken && usertype) {
+    //                 localStorage.setItem('CurrentToken', usertoken);
+    //                 localStorage.setItem('CurrentUserType', usertype);
+    //                 email = "";
+    //                 password = "";
+    //                 if (localStorage.getItem("CurrentUserType") == "user") {
+    //                     location.href = '../../index.html';
+    //                 }
+    //             }
+
+    //             else {
+    //                 alert('error');
+    //             }
+
+    //         },
+    //         error: function (result) {
+    //             alert('error');
+    //         }
+    //     });
+    // }
 });
