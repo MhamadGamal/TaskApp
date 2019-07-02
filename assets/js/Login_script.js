f9b1f2df3494f34b875b02e13123a727eef89537
Mohamed Gamal Masood Abou-alela <Mohamed.G70500@Egypt.TE-Data.core>
2019-07-02 17:00:08 +0200

    fb login

--- assets/js/Login_script.js
+++ assets/js/Login_script.js
@@ -3,50 +3,101 @@ $("#loginbtn").on("click",function (e) {
     let email = $("#usermail").val();
     let password = $("#userpass").val();
     let token = "test";
+
+
+
     if (email && password) {
-        $.ajax({
-            url: "http://88.80.184.99/tasker/web/api/logins",
-            method: 'POST',
-            dataType: "json",
-            async: false,
-            cache: false,
-            timeout: 3000,
-            data: { 'email': email, 'password': password, 'device_token': token },
-            success: function (result) {
-                debugger;
-
-                if (result.error.status==true) {
-                    var message = result.error.message;
-                    $("#alert").fadeIn().html(message);
-                }
-                else {
-
-                    let usertoken = result.data.token;
-                    let usertype = result.data.type;
-                    if (usertoken && usertype) {
-                        localStorage.setItem('CurrentToken', usertoken);
-                        localStorage.setItem('CurrentUserType', usertype);
-                        email = "";
-                        password = "";
-                        if (localStorage.getItem("CurrentUserType") == "user") {
-                            location.href = '../../index.html';
+        var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{5})$/;
+        if(email.match(phoneno)){
+            $.ajax({
+                url: "http://88.80.184.99/tasker/web/api/logins",
+                method: 'POST',
+                dataType: "json",
+                async: false,
+                cache: false,
+                timeout: 3000,
+                data: { 'phone': email, 'password': password},
+                success: function (result) {
+                    debugger;
+    
+                    if (result.error.status==true) {
+                        var message = result.error.message;
+                        $("#alert").fadeIn().html(message);
+                    }
+                    else {
+    
+                        let usertoken = result.data.token;
+                        let usertype = result.data.type;
+                        if (usertoken && usertype) {
+                            localStorage.setItem('CurrentToken', usertoken);
+                            localStorage.setItem('CurrentUserType', usertype);
+                            email = "";
+                            password = "";
+                            if (localStorage.getItem("CurrentUserType") == "user") {
+                                location.href = '../../index.html';
+                            }
+                        }
+    
+                        else {
+                        $("#alert").fadeIn().html(message);
+                        //alert('error');
                         }
                     }
-
-                    else {
-                    $("#alert").fadeIn().html(message);
+                  
+    
+                },
+                error: function (result) {
+                    
+                    $("#alert").fadeIn().html(message);                
                     //alert('error');
-                    }
                 }
-              
-
-            },
-            error: function (result) {
-                
-                $("#alert").fadeIn().html(message);                
-                //alert('error');
-            }
-        });
+            });
+        }
+        else{
+            $.ajax({
+                url: "http://88.80.184.99/tasker/web/api/logins",
+                method: 'POST',
+                dataType: "json",
+                async: false,
+                cache: false,
+                timeout: 3000,
+                data: { 'email': email, 'password': password, 'device_token': token },
+                success: function (result) {
+                    debugger;
+    
+                    if (result.error.status==true) {
+                        var message = result.error.message;
+                        $("#alert").fadeIn().html(message);
+                    }
+                    else {
+    
+                        let usertoken = result.data.token;
+                        let usertype = result.data.type;
+                        if (usertoken && usertype) {
+                            localStorage.setItem('CurrentToken', usertoken);
+                            localStorage.setItem('CurrentUserType', usertype);
+                            email = "";
+                            password = "";
+                            if (localStorage.getItem("CurrentUserType") == "user") {
+                                location.href = '../../index.html';
+                            }
+                        }
+    
+                        else {
+                        $("#alert").fadeIn().html(message);
+                        //alert('error');
+                        }
+                    }
+                  
+    
+                },
+                error: function (result) {
+                    
+                    $("#alert").fadeIn().html(message);                
+                    //alert('error');
+                }
+            });
+        }
     } 
     if(!email){
         $("#usermail").addClass("error")
