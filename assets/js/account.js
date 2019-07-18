5dc04759ecc1cb31b17444ce689c40d20e5401a5
Mohamed Gamal Masood Abou-alela <Mohamed.G70500@Egypt.TE-Data.core>
2019-07-18 23:01:30 +0200

    supplier  profile

--- assets/js/account.js
+++ assets/js/account.js
@@ -92,26 +92,139 @@ $(function(){
     console.log(loginnedUserData);
     let currtoken = localStorage.getItem("CurrentToken")
     $("#userImg").attr('src','http://88.80.184.99/tasker/web/'+loginnedUserData.image)
-    $("#showUserName").text(loginnedUserData.user_name);
-    $("#editUserName input").val(loginnedUserData.user_name);
+    $("#showUserName").text(loginnedUserData.name);
+    $("#editUserName input").val(loginnedUserData.name);
     $("#phone").text(loginnedUserData.phone);
     $("#userEmail").text(loginnedUserData.email);
     $("#userAddress").text(loginnedUserData.info.address);
     $("#editAddress input").val(loginnedUserData.info.address);
+    if(loginnedUserData.setting.language){
+        document.querySelector("#language option[value='"+loginnedUserData.setting.language+"']").selected = true
+    }
+    debugger;
     let emailStatusBtn = document.querySelector(".switch-email input[type='checkbox']");
+    //set email status
+    if(loginnedUserData.setting.email == "true"){
+        emailStatusBtn.checked = true
+    }else{
+        emailStatusBtn.checked = false
 
+    }
+    
     if(loginnedUserData.type == "supplier"){
-        //$("#showBusinessName").text(loginnedUserData.)
+        $("#showBusinessName").text(loginnedUserData.user_name);
+        $("#businessName input").val(loginnedUserData.user_name);
         $("#userDesc").text(loginnedUserData.description)
-        $("#userDiscEdit textarea").val(loginnedUserData.description)
+        $("#userDiscEdit textarea").val(loginnedUserData.description);
+        let chbx = document.querySelector("#activStatus input[type='checkbox']")
+        if(loginnedUserData.worked == 1){   
+            chbx.checked = true
+        }else{
+            chbx.checked = false
+
+        }
+
         let categoriesWrapper = "";
         for(let cat of loginnedUserData.category){
             categoriesWrapper += `
-            <label > ${getCategory(Number(cat)).name_en} </label>
-            `
+            <label id="${getCategory(Number(cat)).id}"> ${getCategory(Number(cat)).name_en} </label>
+            `;
         }
-        $(".categories").html(categoriesWrapper)
-    }
+        $(".categories").html(categoriesWrapper);
+        let CAwrapper="";
+        for( let item of subCategoryArr){
+             
+            let id = String(item.id)
+            if(loginnedUserData.category.includes(id)){
+                CAwrapper += `
+                      
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" checked="checked" class="custom-control-input" value="${item.name_en}" data-id="${item.id}"  id="${item.name_en}${item.id}">
+                    <label class="custom-control-label" for="${item.name_en}${item.id}">${item.name_en} </label>
+                </div>
+            `;
+            }
+            else{
+                CAwrapper += `
+                  
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox"  class="custom-control-input" data-name="${item.name_en}" value="${item.name_en}" data-id="${item.id}"  id="${item.name_en}">
+                        <label class="custom-control-label" for="${item.name_en}">${item.name_en} </label>
+                    </div>
+                `;
+            }
+        }
+        $("#providersCategories .modal-body").html(CAwrapper);
+        $("#editProvCateg").on("click", function(){
+            $("#providersCategories").modal("show");
+        });
+
+
+        //modal
+        $(document).on("click","#SaveCategories", function(e){
+            e.preventDefault();
+             checedArr = [];
+            let els = document.querySelectorAll(".modal-body input[type='checkbox']")
+            els.forEach((el)=>{
+                if( el.checked == true ){  checedArr.push(el) }
+            
+            })
+            console.log(checedArr);
+            let categoriesWrapper = "";
+            for(let cat of checedArr){
+                categoriesWrapper += `
+                <label id="${cat.getAttribute('data-id')}"> ${cat.value} </label>
+                `;
+            }
+            $(".categories").html(categoriesWrapper);
+            $('#providersCategories').modal('hide')
+        });
+
+        //Active stauts
+        let activStatus = 0;
+        $("#activStatus input[type='checkbox']").on("change", function(){
+            debugger;
+            let isWorked = 0;
+            if($(this).is(":checked") == true ){
+                isWorked = 1
+            }
+            debugger;
+            $.ajax({
+                url: "http://88.80.184.99/tasker/web/api/changes/statuses",
+                method: 'POST',
+                dataType: "json",
+                async: false,
+                cache: false,
+                timeout: 30000,
+                data: {
+                    "token": currtoken,
+                    "is_worked": isWorked
+                },
+                success: function (result) {
+                    debugger;
+                    if (result.error.status == true) {
+                        var message = result.error.message;
+                        $(".alert").fadeIn().html(message);                
+                        //alert('error');
+                    }
+                    else {
+    
+                        console.log(result);
+                        //location.reload()
+                        
+                    }
+    
+                },
+                error: function (result) {
+                    $(".alert").fadeIn().html("error");                
+                
+                    //alert('error');
+                }
+            }); 
+    
+        });
+
+    }//supplieeeeeeeeeer
 
     //edit text
     $(document).on("click", "span.edit", function(){
@@ -138,7 +251,7 @@ $(function(){
                      "profile_image": imgBase64
                 },
                 success: function (result) {
-                    debugger;
+                     
                     if (result.error.status == true) {
                         var message = result.error.message;
                         $(".alert").fadeIn().html(message)
@@ -156,20 +269,32 @@ $(function(){
     })
     //save data
     $(document).on("click", "button#save", function(){
-        debugger;
+         debugger;
         let userName = $("#editUserName input").val();
         let address = $("#editAddress input").val();
+        let businessName = $("#businessName input").val();
+        
+        let description = $("#userDiscEdit textarea").val();
         let updatedData;
+        let catWrapper = document.querySelectorAll(".categories label");
+        let category =[];
+        for(let item of catWrapper){
+            category.push(item.getAttribute("id"))
+        }
         if(loginnedUserData.type == "user"){
             updatedData = {
-                "user_name": userName,
+                "name": userName,
                 "address": address,
                 "token": currtoken
             }
         }else{
+
             updatedData = {
-                "user_name": userName,
+                "name": userName,
                 "address": address,
+                "user_name": businessName,
+                "description": description,
+                "category": category,
                 "token": currtoken
             }
         }
@@ -182,7 +307,7 @@ $(function(){
             timeout: 30000,
             data: updatedData,
             success: function (result) {
-                debugger;
+                 
                 if (result.error.status == true) {
                     var message = result.error.message;
                     $(".alert").fadeIn().html(message);                
@@ -205,20 +330,16 @@ $(function(){
     });
 
 
-    //set email status
-    if(loginnedUserData.setting.email == true){
-        emailStatusBtn.checked = true
-    }else{
-        emailStatusBtn.checked = false
-
-    }
+    
 
 
      // setting
 
-     $(".switch input[type='checkbox']").on("change", function(){
+     $(".switch-email input[type='checkbox']").on("change", function(e){
+         e.preventDefault();
+         debugger;
         let emailStatus;
-        if($(".switch input[type='checkbox']").is(":checked")){
+        if(document.querySelector(".switch input[type='checkbox']").checked){
             emailStatus = true;
         }else{
             emailStatus = false;
@@ -240,7 +361,7 @@ $(function(){
                 "token": currtoken
             },
             success: function (result) {
-                debugger;
+                 
                 if (result.error.status == true) {
                     var message = result.error.message;
                     $(".alert").fadeIn().html(message);                
@@ -249,7 +370,7 @@ $(function(){
                 else {
 
                     console.log(result);
-                    location.reload()
+                    //location.reload()
                     
                 }
 
